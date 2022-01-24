eb908a80d49da1048cc8cf50f80a57726c93ea6e
Ayrton Yagami <ayrtonyagami@hotmail.com>
2022-01-24 16:18:01 +0100

    LOGIN E PAINAL

--- GestaoPedidosNotificacao.UI/Controllers/ContaController.cs
+++ GestaoPedidosNotificacao.UI/Controllers/ContaController.cs
@@ -1,17 +1,61 @@
-﻿using System;
+﻿using GestaoPedidosNotificacao.UI.Entities;
+using GestaoPedidosNotificacao.UI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace GestaoPedidosNotificacao.UI.Controllers
 {
     public class ContaController : Controller
     {
+        private GestaoPedidosNotificacaoDBEntities db = new GestaoPedidosNotificacaoDBEntities();
+
         // GET: Conta
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+
+                    var logUsr = db.Utilizadors.FirstOrDefault(x => 
+                        x.Email.ToLower().Equals(model.UserName.ToLower()) &&
+                        x.Senha.Equals(model.Password)
+                    );
+
+                    if (logUsr == null)
+                        throw new Exception("Utilizador sem acesso!");
+
+                    FormsAuthentication.SetAuthCookie(logUsr.Id.ToString(), model.RememberMe);
+
+                    return RedirectToAction("Index","Home");
+                }
+
+                ModelState.AddModelError("", "Utilizador sem acesso!");
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View();
+            }
         }
     }
 }
---
+++ GestaoPedidosNotificacao.UI/Models/PedidoModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestaoPedidosNotificacao.UI.Entities
+{
+    public partial class Pedido
+    {
+        public int TempoFactura { get; set; }
+        public int TempoPagamento { get; set; }
+
+        private void CalcularTempo()
+        {
+            CalcularTempoFactura();
+            CalcularTempoPagamento();
+        }
+
+        private void CalcularTempoFactura()
+        {
+            if (this.DataFactura == null) return;
+            this.TempoFactura = (int)this.DataFactura.Value.Subtract(DateTime.Today).TotalDays;
+        }
+        private void CalcularTempoPagamento()
+        {
+            if (this.DataPagamento== null) return;
+            this.TempoFactura = (int)this.DataPagamento.Value.Subtract(DateTime.Today).TotalDays;
+        }
+    }
+}
