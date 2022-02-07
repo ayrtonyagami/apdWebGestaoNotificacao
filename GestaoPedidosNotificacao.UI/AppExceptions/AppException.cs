a2ecf2d904d5d34f6a63c6e2037bd1846473ddaf
Ayrton Yagami <ayrtonyagami@hotmail.com>
2022-02-07 15:28:13 +0100

    Folder App Exceptions

---
+++ GestaoPedidosNotificacao.UI/AppExceptions/AppException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestaoPedidosNotificacao.UI.AppExceptions
+{
+    public class AppException : Exception
+    {
+        public AppException() : base()
+        {
+        }
+        public AppException(string message) : base(message)
+        {
+        }
+    }
+
+    public class AppUnautorizatedException : AppException
+    {
+        public AppUnautorizatedException() : base("Utilizador Sem Acesso.")
+        {
+        }
+    }
+}
